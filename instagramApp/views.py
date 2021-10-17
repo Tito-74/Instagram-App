a4ceca9486d0182eaabfb9c7ff5c0a220eb7b3da
Tito-74 <kipkirui133@gmail.com>
2021-10-17 12:33:34 +0300

    adding  required imports for regestration

--- instagramApp/views.py
+++ instagramApp/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from instagramApp.models import Post,Profile,Comments
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        form = UserCreationForm()
+        if request.method == 'POST':
+            form =UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                # messages.success(request,'Account was created successfully')
+                return redirect('login')
+        context = {'form': form}
+        return render(request,'registration/registration_form.html',  context)
+
+
 def index(request):
     posts = Post.objects.all()
     profile = Profile.objects.all()
